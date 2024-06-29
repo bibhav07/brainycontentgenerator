b8fe62fadbd19ca582ad435dc4a1f1adb48658d6
Bibhav Yadav <managementlucky07@gmail.com>
2024-06-29 10:32:55 +0530

    feat: added active page sidebar

--- app/dashboard/_components/SideNav.tsx
+++ app/dashboard/_components/SideNav.tsx
@@ -1,8 +1,16 @@
+"use client";
 import { FileClock, Home, SettingsIcon, Wallet } from "lucide-react";
 import Image from "next/image";
-import React from "react";
+import { usePathname } from "next/navigation";
+import React, { useEffect } from "react";
 
 function SideNav() {
+  const path = usePathname();
+
+  useEffect(() => {
+    console.log(path);
+  }, []);
+
   const MenuList = [
     {
       name: "Home",
@@ -34,7 +42,11 @@ function SideNav() {
 
       <div className="mt-10">
         {MenuList.map((menu, index) => (
-          <div className="flex gap-2 mb-2 p-3 hover:bg-primary hover:text-white rounded-lg">
+          <div
+            className={`flex gap-2 mb-2 p-3 hover:bg-primary hover:text-white rounded-lg cursor-pointer items-center ${
+              path == menu.path && "bg-primary text-white"
+            }`}
+          >
             <menu.icon />
             <h2>{menu.name}</h2>
           </div>
